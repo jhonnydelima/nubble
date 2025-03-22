04692b2678989041e85d1d78ce1b20774dba806a
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-22 16:26:52 -0300

    feat: create project theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { Text } from './src/components/Text/Text';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from './src/theme/theme';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Text style={{color: 'red'}}>React Native</Text>
-      <Text variant="headingLarge">React Native</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text style={{color: 'red'}}>React Native</Text>
+        <Text variant="headingLarge">React Native</Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
