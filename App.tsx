848396f35d821377e194f86d9fb1443bdc76f9c3
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-23 00:39:18 -0300

    feat: add button disabled preset

--- App.tsx
+++ App.tsx
@@ -14,8 +14,10 @@ function App(): React.JSX.Element {
           <Text preset="headingLarge" fontStyle="italic">React Native</Text>
           <Button title="Primary" />
           <Button title="Primary Loading" loading />
+          <Button title="Primary Disabled" disabled />
           <Button title="Outline" preset="outline" />
           <Button title="Outline Loading" preset="outline" loading />
+          <Button title="Outline Disabled" preset="outline" disabled />
         </Box>
       </SafeAreaView>
     </ThemeProvider>
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,20 +4,23 @@ import {Text} from '../Text/Text';
 import { ButtonPreset, buttonPresets } from './buttonPresets';
 
 interface ButtonProps extends TouchableOpacityBoxProps {
+  preset?: ButtonPreset;
   title: string;
   loading?: boolean;
-  preset?: ButtonPreset;
+  disabled?: boolean;
 }
 
 export function Button({
+  preset = 'primary',
   title,
   loading,
-  preset = 'primary',
+  disabled,
   ...touchableOpacityBoxProps
 }: ButtonProps) {
-  const buttonPreset = buttonPresets[preset];
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
   return (
     <TouchableOpacityBox
+      disabled={disabled || loading}
       paddingHorizontal="s20"
       height={50}
       justifyContent="center"
