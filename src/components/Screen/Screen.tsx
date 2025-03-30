6f5d38bb9064b7be67612c27b16a6dd7817b5a24
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-30 20:33:43 -0300

    refactor: update `Screen` component

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,13 +1,40 @@
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { useAppSafeArea } from '../../hooks/useAppSafeArea';
 import { Box } from '../Box/Box';
+import { Icon } from '../Icon/Icon';
+import { Text } from '../Text/Text';
+import { useAppTheme } from '../../hooks/useAppTheme';
 
 interface ScreenProps {
   children: React.ReactNode;
+  canGoBack?: boolean;
+  scrollable?: boolean;
 }
 
-export function Screen({ children }: ScreenProps) {
-  const { top } = useAppSafeArea();
+export function Screen({ children, canGoBack = false, scrollable = false }: ScreenProps) {
+  const { top, bottom } = useAppSafeArea();
+  const { colors } = useAppTheme();
+  const Container = scrollable ? ScrollView : Box;
   return (
-    <Box px="s24" style={{ paddingTop: top }}>{children}</Box>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Container
+        style={{
+          backgroundColor: colors.background,
+          height: '100%',
+        }}
+      >
+        <Box px="s24" style={{paddingTop: top, paddingBottom: bottom}}>
+          {canGoBack && (
+            <Box flexDirection="row" mb="s24" alignItems="center">
+              <Icon name="arrowLeft" color="primary" />
+              <Text preset="paragraphMedium" bold ml="s8">
+                Voltar
+              </Text>
+            </Box>
+          )}
+          {children}
+        </Box>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
