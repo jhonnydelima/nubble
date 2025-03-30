0fb3ebe8cdc595b1cf921008c5b282972c77b5fd
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-30 01:13:47 -0300

    feat: add `LoginScreen` screen

--- App.tsx
+++ App.tsx
@@ -1,47 +1,12 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
-import { Text } from './src/components/Text/Text';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
-import { Box } from './src/components/Box/Box';
-import { Button } from './src/components/Button/Button';
-import { TextInput } from './src/components/TextInput/TextInput';
-import { Icon } from './src/components/Icon/Icon';
+import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <Box px="s24">
-          <Text preset="headingLarge" mb="s8">Olá</Text>
-          <Text preset="paragraphLarge" mb="s40">
-            Digite seu e-mail e senha para entrar
-          </Text>
-
-            <TextInput
-              label="E-mail"
-              placeholder="Digite seu e-mail"
-              errorMessage="mensagem de erro"
-              boxProps={{mb: 's20'}}
-            />
-
-            <TextInput
-              label="Senha"
-              placeholder="Digite sua senha"
-              RightComponent={
-                <Icon name="eyeOn" color="gray2" />
-              }
-              boxProps={{mb: 's10'}}
-            />
-
-          <Text preset="paragraphSmall" color="primary" bold>
-            Esqueci minha senha
-          </Text>
-
-          <Button mt="s48" title="Entrar" />
-          <Button preset="outline" mt="s12" title="Criar uma conta" />
-        </Box>
-      </SafeAreaView>
+      <LoginScreen />
     </ThemeProvider>
   );
 }
