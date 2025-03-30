99f70b92724601b13215615e3cd186cb761ea47b
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-30 20:34:04 -0300

    feat: add `SignUpScreen`

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from '@shopify/restyle';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { theme } from './src/theme/theme';
 import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
+import { SignUpScreen } from './src/screens/auth/SignUpScreen/SignUpScreen';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        {/* <LoginScreen /> */}
+        <SignUpScreen />
       </ThemeProvider>
     </SafeAreaProvider>
   );
