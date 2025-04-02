44a346e8548e8b1c5eaf402229463970f4cb039f
Jhonny Lima <lima.jhonny95@gmail.com>
2025-04-01 23:26:46 -0300

    feat: create `Router` file

--- App.tsx
+++ App.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { ThemeProvider } from '@shopify/restyle';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { theme } from './src/theme/theme';
-import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
-import { SignUpScreen } from './src/screens/auth/SignUpScreen/SignUpScreen';
+import { Router } from './src/routes/Routes';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        {/* <LoginScreen /> */}
-        <SignUpScreen />
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
