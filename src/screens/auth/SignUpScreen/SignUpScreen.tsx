6c9a2b495ee96561f182f1ae1fc891a9d6497964
Jhonny Lima <lima.jhonny95@gmail.com>
2025-04-03 22:45:15 -0300

    feat: add typescript to navigation

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -1,10 +1,14 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Button } from '../../../components/Button/Button';
 import { PasswordInput } from '../../../components/PasswordInput/PasswordInput';
 import { Screen } from '../../../components/Screen/Screen';
 import { Text } from '../../../components/Text/Text';
 import { TextInput } from '../../../components/TextInput/TextInput';
+import { RootStackParamList } from '../../../routes/Routes';
 
-export function SignUpScreen() {
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
+
+export function SignUpScreen(props: ScreenProps) {
   function submitForm() {
     // TODO: Implementar a lógica de criação de conta
   }
