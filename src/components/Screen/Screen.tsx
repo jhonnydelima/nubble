94a9f7331bf512e85a5b1a4278c1ca7ec10de0ea
Jhonny Lima <lima.jhonny95@gmail.com>
2025-04-07 23:48:09 -0300

    fix: fix screen click error

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,11 +1,11 @@
-import { KeyboardAvoidingView, Platform } from 'react-native';
-import { useAppSafeArea } from '../../hooks/useAppSafeArea';
-import { Box, TouchableOpacityBox } from '../Box/Box';
-import { Icon } from '../Icon/Icon';
-import { Text } from '../Text/Text';
-import { useAppTheme } from '../../hooks/useAppTheme';
-import { useNavigation } from '@react-navigation/native';
-import { ScrollViewContainer, ViewContainer } from './components/ScreenContainer';
+import {KeyboardAvoidingView, Platform} from 'react-native';
+import {useAppSafeArea} from '../../hooks/useAppSafeArea';
+import {Box, TouchableOpacityBox} from '../Box/Box';
+import {Icon} from '../Icon/Icon';
+import {Text} from '../Text/Text';
+import {useAppTheme} from '../../hooks/useAppTheme';
+import {useNavigation} from '@react-navigation/native';
+import {ScrollViewContainer, ViewContainer} from './components/ScreenContainer';
 
 interface ScreenProps {
   children: React.ReactNode;
@@ -13,9 +13,13 @@ interface ScreenProps {
   scrollable?: boolean;
 }
 
-export function Screen({ children, canGoBack = false, scrollable = false }: ScreenProps) {
-  const { top, bottom } = useAppSafeArea();
-  const { colors } = useAppTheme();
+export function Screen({
+  children,
+  canGoBack = false,
+  scrollable = false,
+}: ScreenProps) {
+  const {top, bottom} = useAppSafeArea();
+  const {colors} = useAppTheme();
   const navigation = useNavigation();
   const Container = scrollable ? ScrollViewContainer : ViewContainer;
 
@@ -28,24 +32,24 @@ export function Screen({ children, canGoBack = false, scrollable = false }: Scre
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-      style={{ flex: 1 }}
-    >
+      style={{flex: 1}}>
       <Container backgroundColor={colors.background}>
-        <TouchableOpacityBox
-          onPress={handleGoBack}
-          px="s24"
-          style={{paddingTop: top, paddingBottom: bottom}}
-        >
+        <Box px="s24" style={{paddingTop: top, paddingBottom: bottom}}>
           {canGoBack && (
-            <Box flexDirection="row" mb="s24" alignItems="center">
+            <TouchableOpacityBox
+              onPress={handleGoBack}
+              flexDirection="row"
+              mb="s24"
+              alignItems="center"
+            >
               <Icon name="arrowLeft" color="primary" />
               <Text preset="paragraphMedium" bold ml="s8">
                 Voltar
               </Text>
-            </Box>
+            </TouchableOpacityBox>
           )}
           {children}
-        </TouchableOpacityBox>
+        </Box>
       </Container>
     </KeyboardAvoidingView>
   );
