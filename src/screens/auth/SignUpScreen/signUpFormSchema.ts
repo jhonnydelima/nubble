6930c2181e7f4e31724a7432cacd2a1598cb72c2
Jhonny Lima <lima.jhonny95@gmail.com>
2025-04-10 01:01:55 -0300

    refactor: add zod schema validation to `LoginScreen`

--- src/screens/auth/SignUpScreen/signUpFormSchema.ts
+++ src/screens/auth/SignUpScreen/signUpFormSchema.ts
@@ -6,16 +6,16 @@ export const signUpFormSchema = z.object({
   username: z.string().regex(userNameRegex, 'username inválido').toLowerCase(),
   fullName: z
     .string()
-    .min(5, 'Nome completo obrigatório')
-    .max(50, 'Nome completo deve ter no máximo 50 caracteres')
+    .min(5, 'nome completo obrigatório')
+    .max(50, 'nome completo deve ter no máximo 50 caracteres')
     .transform(value => {
       return value
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase)
         .join(' ');
     }),
-  email: z.string().email('E-mail inválido'),
-  password: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres'),
+  email: z.string().email('e-mail inválido'),
+  password: z.string().min(8, 'senha deve ter no mínimo 8 caracteres'),
 });
 
 export type SignUpFormType = z.infer<typeof signUpFormSchema>;
