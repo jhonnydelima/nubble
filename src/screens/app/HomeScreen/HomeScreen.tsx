979892f9f12ed977b4eb2d9dda008c4034656868
Jhonny Lima <lima.jhonny95@gmail.com>
2025-04-20 19:35:58 -0300

    feat: add flatlist to home screen

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from 'react';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
 import {Post, postService} from '@domain';
 
-import {Screen, Text} from '@components';
+import {Box, Screen, Text} from '@components';
 import {AppBottomTabScreenProps} from '@routes';
 
 export function HomeScreen({
@@ -15,11 +16,32 @@ export function HomeScreen({
     postService.getList().then(list => setPostList(list));
   }, []);
 
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box mb="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 32, height: 32}}
+          />
+          <Text>{item.author.userName}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          style={{width: Dimensions.get('screen').width, height: 300}}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      {postList.map(post => (
-        <Text key={post.id}>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }
