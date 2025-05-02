5441a95bf50bd19580248f836789742b2818c45a
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-02 00:27:00 -0300

    refactor: get post from backend using fetch api

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,7 +2,17 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer MQ.LfuWozFZMG_KTl_5w45sALiEPHRP08XuKBGrdB7Ag-C1FKib33qquS7g_lhv',
+    },
+  });
+
+  let data = await response.json();
+  console.log(data);
+  // await new Promise(resolve => setTimeout(resolve, 1000));
   return postListMock;
 }
 
