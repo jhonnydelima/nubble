b5fb61d4b56db5583c8c3200ce39765f7d964f69
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-03 18:37:59 -0300

    feat: create `postAdapter` and refactor post service

---
+++ src/domain/Post/postAdapter.ts
@@ -0,0 +1,27 @@
+import {Post, PostAPI} from './postTypes';
+
+/**
+ * @description This function is used to convert the API response to a Post object.
+ * It takes a PostAPI object as input and returns a Post object.
+ * @param {PostAPI} postAPI - The PostAPI object to be converted.
+ * @returns {Post} - The converted Post object.
+ */
+function toPost(postAPI: PostAPI): Post {
+  return {
+    id: postAPI.id.toString(),
+    text: postAPI.text,
+    author: {
+      profileURL: postAPI.user.profile_url,
+      name: postAPI.user.full_name,
+      userName: postAPI.user.username,
+    },
+    imageURL: postAPI.image_url,
+    reactionCount: Number(postAPI.meta.like_count),
+    commentCount: Number(postAPI.meta.comments_count),
+    favoriteCount: Number(postAPI.meta.favorite_count),
+  };
+}
+
+export const postAdapter = {
+  toPost,
+};
--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,8 +1,10 @@
+import {postAdapter} from './postAdapter';
 import {postApi} from './postApi';
 import {Post} from './postTypes';
 
 async function getList(): Promise<Post[]> {
-  const postList = await postApi.getList();
+  const apiResponse = await postApi.getList();
+  const postList = apiResponse.data.map(postAdapter.toPost);
   return postList;
 }
 
