fe4e93c0ba263ab986ff3da3acd0bc40374eb953
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-22 21:56:59 -0300

    feat: create `postCommentAdapter`

---
+++ src/domain/PostComment/postCommentAdapter.ts
@@ -0,0 +1,25 @@
+import {PostComment, PostCommentAPI} from './postCommentTypes';
+
+/**
+ * @description This function is used to convert the API response to a PostComment object.
+ * It takes a PostCommentAPI object as input and returns a PostComment object.
+ * @param {PostCommentAPI} postCommentAPI - The PostCommentAPI object to be converted.
+ * @returns {PostComment} - The converted PostComment object.
+ */
+function toPostComment(postCommentAPI: PostCommentAPI): PostComment {
+  return {
+    id: postCommentAPI.id,
+    message: postCommentAPI.message,
+    created_at: postCommentAPI.created_at,
+    author: {
+      id: postCommentAPI.user.id,
+      profileURL: postCommentAPI.user.profile_url,
+      name: postCommentAPI.user.full_name,
+      userName: postCommentAPI.user.username,
+    },
+  };
+}
+
+export const postCommentAdapter = {
+  toPostComment,
+};
