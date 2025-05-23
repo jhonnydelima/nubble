8b3efac482a16e77c54b42bd9327e6deb6b59173
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-22 21:56:22 -0300

    feat: create `postCommentApi`

---
+++ src/domain/PostComment/postCommentApi.ts
@@ -0,0 +1,23 @@
+import {api, PageAPI, PageParams} from '@api';
+
+import {PostCommentAPI} from './postCommentTypes';
+
+async function getList(
+  postId: number,
+  pageParams?: PageParams,
+): Promise<PageAPI<PostCommentAPI>> {
+  const response = await api.get<PageAPI<PostCommentAPI>>(
+    '/user/post_comment',
+    {
+      params: {
+        postId,
+        ...pageParams,
+      },
+    },
+  );
+  return response.data;
+}
+
+export const postCommentApi = {
+  getList,
+};
