8ee39f69ebed78d2f11c210371adcf0a882ca7da
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-22 21:54:37 -0300

    refactor: update temporary token

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization:
-      'Bearer MQ.LfuWozFZMG_KTl_5w45sALiEPHRP08XuKBGrdB7Ag-C1FKib33qquS7g_lhv',
+      'Bearer MQ.08nQMDHxfTiuD1cTiQxYBfwkGJhaIHkfIPPcKQzDb4p5GVZmuaA9s__xDV8U',
   },
 });
