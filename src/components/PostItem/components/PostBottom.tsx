9e10a9d8970201c61644cd4f68b1fa5007cf3ca5
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-24 22:52:10 -0300

    refactor: add navigation to `PostCommentsScreen`

--- src/components/PostItem/components/PostBottom.tsx
+++ src/components/PostItem/components/PostBottom.tsx
@@ -1,11 +1,19 @@
 import {Post} from '@domain';
+import {useNavigation} from '@react-navigation/native';
 
 import {Box, Text} from '@components';
 
-type Props = Pick<Post, 'author' | 'text' | 'commentCount'>;
+type Props = Pick<Post, 'author' | 'text' | 'commentCount' | 'id'>;
 
-export function PostBottom({author, text, commentCount}: Props) {
+export function PostBottom({author, text, commentCount, id}: Props) {
+  const navigation = useNavigation();
   const commentText = getCommentText(commentCount);
+
+  function navigateToPostCommentsScreen() {
+    navigation.navigate('PostCommentsScreen', {
+      postId: id,
+    });
+  }
 
   return (
     <Box mt="s16">
@@ -16,7 +24,12 @@ export function PostBottom({author, text, commentCount}: Props) {
         {text}
       </Text>
       {commentText && (
-        <Text preset="paragraphSmall" bold color="primary" mt="s8">
+        <Text
+          onPress={navigateToPostCommentsScreen}
+          preset="paragraphSmall"
+          bold
+          color="primary"
+          mt="s8">
           {commentText}
         </Text>
       )}
