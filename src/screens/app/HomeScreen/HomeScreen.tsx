140f4834c411c7ae3f88781f1eb44cea16f60780
Jhonny Lima <lima.jhonny95@gmail.com>
2025-05-27 22:50:18 -0300

    fix: fix typescript errors

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -40,7 +40,7 @@ export function HomeScreen({
       <FlatList
         ref={flatListRef}
         data={postList}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id.toString()}
         renderItem={renderItem}
         onEndReached={fetchNextPage}
         onEndReachedThreshold={0.1}
