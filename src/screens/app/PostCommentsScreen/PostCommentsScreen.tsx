4f4990e9d2fd3ac1ccf5a80d2d9ea85ac229840d
Jhonny Lima <lima.jhonny95@gmail.com>
2025-06-03 00:08:00 -0300

    refactor: update flatlist from `PostCommentsScreen`

--- src/screens/app/PostCommentsScreen/PostCommentsScreen.tsx
+++ src/screens/app/PostCommentsScreen/PostCommentsScreen.tsx
@@ -3,15 +3,18 @@ import {FlatList, ListRenderItemInfo} from 'react-native';
 import {PostComment, usePostCommentList} from '@domain';
 
 import {Screen} from '@components';
+import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
 
 import {PostCommentItem} from './components/PostCommentItem';
+import {PostCommentsBottom} from './components/PostCommentsBottom';
 
 export function PostCommentsScreen({
   route,
 }: AppScreenProps<'PostCommentsScreen'>) {
   const postId = route.params.postId;
-  const {data} = usePostCommentList(postId);
+  const {data, fetchNextPage, hasNextPage} = usePostCommentList(postId);
+  const {bottom} = useAppSafeArea();
 
   function renderItem({item}: ListRenderItemInfo<PostComment>) {
     return <PostCommentItem postComment={item} />;
@@ -19,7 +22,18 @@ export function PostCommentsScreen({
 
   return (
     <Screen title="Comentários" canGoBack>
-      <FlatList data={data} renderItem={renderItem} />
+      <FlatList
+        data={data}
+        showsVerticalScrollIndicator={false}
+        renderItem={renderItem}
+        contentContainerStyle={{paddingBottom: bottom + 16}}
+        ListFooterComponent={
+          <PostCommentsBottom
+            fetchNextPage={fetchNextPage}
+            hasNextPage={hasNextPage}
+          />
+        }
+      />
     </Screen>
   );
 }
