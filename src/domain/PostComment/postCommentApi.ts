4a06fc72572d403abf4e5e88c3419bcdc331ea43
Jhonny Lima <lima.jhonny95@gmail.com>
2025-06-18 23:59:21 -0300

    feat: create `remove` function in `postCommentApi`

--- src/domain/PostComment/postCommentApi.ts
+++ src/domain/PostComment/postCommentApi.ts
@@ -2,12 +2,14 @@ import {api, PageAPI, PageParams} from '@api';
 
 import {PostCommentAPI} from './postCommentTypes';
 
+const POST_COMMENT_BASE_URL = '/user/post_comment';
+
 async function getList(
   postId: number,
   pageParams?: PageParams,
 ): Promise<PageAPI<PostCommentAPI>> {
   const response = await api.get<PageAPI<PostCommentAPI>>(
-    '/user/post_comment',
+    POST_COMMENT_BASE_URL,
     {
       params: {
         post_id: postId,
@@ -22,14 +24,22 @@ async function create(
   postId: number,
   message: string,
 ): Promise<PostCommentAPI> {
-  const response = await api.post<PostCommentAPI>('/user/post_comment', {
+  const response = await api.post<PostCommentAPI>(POST_COMMENT_BASE_URL, {
     post_id: postId,
     message,
   });
   return response.data;
 }
 
+async function remove(postCommentId: number): Promise<{message: string}> {
+  const response = await api.delete<{message: string}>(
+    `${POST_COMMENT_BASE_URL}/${postCommentId}`,
+  );
+  return response.data;
+}
+
 export const postCommentApi = {
   getList,
   create,
+  remove,
 };
