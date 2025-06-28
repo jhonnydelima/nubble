68a5e4523b610c050a5e6b95988e4e88a834874a
Jhonny Lima <lima.jhonny95@gmail.com>
2025-06-27 23:57:11 -0300

    refactor: update domain imports

---
+++ src/domain/PostComment/index.ts
@@ -0,0 +1,5 @@
+export * from './postCommentService';
+export * from './postCommentTypes';
+export * from './useCases/usePostCommentCreate';
+export * from './useCases/usePostCommentList';
+export * from './useCases/usePostCommentRemove';
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,14 +1,3 @@
-// Post
-export * from './Post/postTypes';
-export * from './Post/postService';
-export * from './Post/useCases/usePostList';
-
-// PostComment
-export * from './PostComment/postCommentTypes';
-export * from './PostComment/postCommentService';
-export * from './PostComment/useCases/usePostCommentList';
-export * from './PostComment/useCases/usePostCommentCreate';
-export * from './PostComment/useCases/usePostCommentRemove';
-
-// Auth
-export * from './Auth/hooks/useUser';
+export * from './Auth';
+export * from './Post';
+export * from './PostComment';
