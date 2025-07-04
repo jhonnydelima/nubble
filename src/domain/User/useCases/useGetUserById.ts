06deded1ad731bfa7cd9e055c771462d92efe927
Jhonny Lima <lima.jhonny95@gmail.com>
2025-07-03 23:51:24 -0300

    refactor: add `staleTime` & `retry` to `useGetUserById` hook

--- src/domain/User/useCases/useGetUserById.ts
+++ src/domain/User/useCases/useGetUserById.ts
@@ -7,6 +7,9 @@ export function useGetUserById(userId: number) {
   const {data, isError, isLoading, refetch} = useQuery({
     queryKey: [QueryKeys.GetUserById, userId],
     queryFn: () => userService.getById(userId),
+    staleTime: 1000 * 30, // 30 seconds
+    // cacheTime: 1000 * 60 * 5, // 5 minutes
+    retry: 1, // Retry once on failure
   });
 
   return {
